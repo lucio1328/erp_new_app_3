abf3ffd5ea75ebcda4903f49897a6a2d7f343fbc
Lucio Rakotoniaina <153309163+lucio1328@users.noreply.github.com>
2025-05-31 12:03:13 +0200

    controller et view pour afficher liste employes

---
+++ src/main/java/com/lucio/erp_new_app_3/controllers/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.lucio.erp_new_app_3.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lucio.erp_new_app_3.dtos.employee.Employee;
+import com.lucio.erp_new_app_3.services.employee.EmployeeService;
+import com.lucio.erp_new_app_3.utils.EnvoyeInformation;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+
+    @GetMapping
+    public ModelAndView showEmployeesPage(HttpSession session) {
+        String sessionCookie = (String) session.getAttribute("sid");
+        ModelAndView modelAndView = new ModelAndView("layout/modele");
+
+        if (sessionCookie == null) {
+            modelAndView.setViewName("redirect:/");
+            return modelAndView;
+        }
+
+        List<Employee> employees = employeeService.getAllEmployees(sessionCookie);
+        modelAndView.addObject("employees", employees);
+
+        EnvoyeInformation.afficherName(session, modelAndView);
+        EnvoyeInformation.setInfo(modelAndView, "Liste des employés", "pages/employee/liste");
+
+        return modelAndView;
+    }
+}
+
