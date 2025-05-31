f7a65f12551d436982a32e6fc4c20fcdef23bed7
Lucio Rakotoniaina <153309163+lucio1328@users.noreply.github.com>
2025-05-31 12:40:08 +0200

    liste avec pagination

--- src/main/java/com/lucio/erp_new_app_3/controllers/EmployeeController.java
+++ src/main/java/com/lucio/erp_new_app_3/controllers/EmployeeController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lucio.erp_new_app_3.dtos.employee.Employee;
 import com.lucio.erp_new_app_3.services.employee.EmployeeService;
 import com.lucio.erp_new_app_3.utils.EnvoyeInformation;
+import com.lucio.erp_new_app_3.utils.PaginationUtils;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -23,7 +25,9 @@ public class EmployeeController {
 
 
     @GetMapping
-    public ModelAndView showEmployeesPage(HttpSession session) {
+    public ModelAndView listeEmployes(HttpSession session,
+                                    @RequestParam(defaultValue = "0") int page,
+                                    @RequestParam(defaultValue = "10") int size) {
         String sessionCookie = (String) session.getAttribute("sid");
         ModelAndView modelAndView = new ModelAndView("layout/modele");
 
@@ -32,8 +36,13 @@ public class EmployeeController {
             return modelAndView;
         }
 
-        List<Employee> employees = employeeService.getAllEmployees(sessionCookie);
-        modelAndView.addObject("employees", employees);
+        List<Employee> allEmployees = employeeService.getAllEmployees(sessionCookie);
+        List<Employee> paginatedEmployees = PaginationUtils.paginate(allEmployees, page, size);
+        int totalPages = PaginationUtils.getTotalPages(allEmployees.size(), size);
+
+        modelAndView.addObject("employees", paginatedEmployees);
+        modelAndView.addObject("currentPage", page);
+        modelAndView.addObject("totalPages", totalPages);
 
         EnvoyeInformation.afficherName(session, modelAndView);
         EnvoyeInformation.setInfo(modelAndView, "Liste des employés", "pages/employee/liste");
